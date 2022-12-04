ac94c814f020614a3d8f78ee17f95668dc951975
unknown <ali-takafi97@hotmail.it>
2022-12-04 19:13:08 +0100

    now able to get data of any number of images using randomly generated id

--- images_request.py
+++ images_request.py
@@ -1,24 +1,40 @@
 import requests
-from urllib.request import urlretrieve
-from pprint import PrettyPrinter
-# import jsonirt
 import psycopg2
 import socket
+from random import choice
+from string import digits
 
-pp = PrettyPrinter()
-
-def request_image():
+def request_image(image_id):
   API_Root = "https://images-api.nasa.gov/search"
   parameters = {
-      'media_type' : 'image'
+      'media_type' : 'image',
+      'nasa_id' : image_id
   }
   response = requests.get(API_Root, params=parameters).json()
-  # return pp.pprint(response)
   return response
+
+# to test what happens when the requested
+# data (PIA00000) is not available: no error
+# but field of info is empty
+
+# def request_image():
+#   API_Root = "https://images-api.nasa.gov/search"
+#   parameters = {
+#       'media_type' : 'image',
+#       'nasa_id' : 'PIA00000'
+#   }
+#
+#   response = requests.get(API_Root, params=parameters).json()
+#   return response
+#
+# data = request_image()
+# print(data)
 
 server = socket.gethostbyname(socket.gethostname())
 
-data = request_image()
+# data = request_image('PIA00001')['collection']['items'][0]
+
+# print(data)
 
 try:
     connection = psycopg2.connect(user="postgres",
@@ -28,24 +44,46 @@ try:
                                   database="nasa")
     
     if connection:
-      print('Connection established')
+      print('\n\n\n','Connection established')
     
     cursor = connection.cursor()
 
-    for key in data['collection']['items']:
-      postgres_insert_query = "INSERT INTO data (id, title, description, image_link) VALUES (%s,%s,%s,%s)"
-      record_to_insert = (key['data'][0]['nasa_id'], key['data'][0]['title'], key['data'][0]['description'], key['links'][0]['href'])
-      cursor.execute(postgres_insert_query, record_to_insert)
+    count = 0
 
+    while count<500:
+      try:
+          random_number = ''.join(choice(digits) for i in range(5))
+          image_id = 'PIA'+random_number
+          # image_id = 'PIA0000'+str(count)
+          print(image_id)
+          data = request_image(image_id)
+          #key = data['collection']['items'][0]
+          id = data['collection']['items'][0]['data'][0]['nasa_id']
+          title = data['collection']['items'][0]['data'][0]['title']
+          description = data['collection']['items'][0]['data'][0]['description']
+          href = data['collection']['items'][0]['links'][0]['href']
+          postgres_insert_query = "INSERT INTO data (id, title, description, image_link) VALUES (%s,%s,%s,%s)"
+          record_to_insert = (id, title, description, href)
+          cursor.execute(postgres_insert_query, record_to_insert)
+      
+      except:
+        print('Data non available')
+
+      finally:
+        count = count + 1
 
 except psycopg2.Error as error:
-    print("Connection error:", error.pgerror)
+    print('\n\n\n',"Connection error:", error.pgerror)
 
 finally:
   connection.commit()
   cursor.close()
   connection.close()
-  print('Connection closed')
+  print('\n\n\n','Connection closed')
+
+
+
+
 
 # image_info = {
 #   'id' : [],
